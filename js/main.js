368c749e3fbbc19fff14023b6ae59895288d278c
nellrcs <nellrcs@gmail.com>
2015-02-23 16:53:41 -0300

    config js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* NOMES FUNCOES EVENTOS AJAX */
+/* success */
+function enviado(data)
+{
+	console.log(data);
+}
+/* error */
+function erro(data)
+{
+	console.log(data);
+}
+/*  beforeSend */
+function antes_enviar(data)
+{
+	console.log(data);
+} 
+/* complete */
+function completo(data)
+{
+	console.log(data);	
+}
